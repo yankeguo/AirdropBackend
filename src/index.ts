598c46598a4e17bd5bd395a91ace5a885ff843d1
GUO YANKE <hi@yankeguo.com>
2024-07-12 14:03:03 +0800

    refactor: format code

--- src/index.ts
+++ src/index.ts
@@ -169,9 +169,11 @@ app.get('/airdrop/list', async (c) => {
 
 	const db = useDatabase(c);
 
-	const records = userIds.length ? await db.query.tAirdrops.findMany({
-		where: (users, { inArray }) => inArray(users.user_id, userIds),
-	}) : [];
+	const records = userIds.length
+		? await db.query.tAirdrops.findMany({
+				where: (users, { inArray }) => inArray(users.user_id, userIds),
+			})
+		: [];
 
 	const result = NFTS.map((nft) => {
 		let is_eligible = false;
@@ -223,7 +225,7 @@ export default {
 	async fetch(req: Request, env: Bindings, ctx: ExecutionContext): Promise<Response> {
 		return app.fetch(req, env, ctx);
 	},
-	async scheduled(event: ScheduledEvent, env: Bindings, ctx: ExecutionContext) { },
+	async scheduled(event: ScheduledEvent, env: Bindings, ctx: ExecutionContext) {},
 	async queue(batch: MessageBatch, env: Bindings, ctx: ExecutionContext): Promise<void> {
 		batch.ackAll();
 	},
